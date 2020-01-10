caf1a25d6f9184b29fec21b2d3a96072e898a9a9
Jacob Graves <jake@hashidev.com>
2020-01-10 16:18:07 -0700

    Cleans up definitions. Adds HP.

--- Sources/PhonyKit/Phony.swift
+++ Sources/PhonyKit/Phony.swift
@@ -1,8 +1,23 @@
 public class Phony {
+    public enum DefinitionsType {
+        case `default`
+        case harryPotter
+        case custom(Definitions)
+    }
+    
     public static var `default`: Phony = Phony()
-    public var definitions: Definitions
+    public var definitions: Definitions = DefaultDefinitions()
 
-    private init() {
-        self.definitions = DefaultDefinitions()
+    private init() {}
+    
+    public static func setDefaultDefinitions(to def: DefinitionsType) {
+        switch def {
+        case .default:
+            Phony.default.definitions = DefaultDefinitions()
+        case .harryPotter:
+            Phony.default.definitions = HPDefinitions()
+        case let .custom(definitions):
+            Phony.default.definitions = definitions
+        }
     }
 }
