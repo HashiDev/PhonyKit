6125f59429f58a59e0a913641329a2c7349e2512
Jacob Graves <jake@hashi.dev>
2021-10-29 12:23:33 -0600

    These functions aren't used.

--- Sources/PhonyKit/Address.swift
+++ Sources/PhonyKit/Address.swift
@@ -78,62 +78,10 @@ public extension Phony {
         return self.definitions.stateAbbr.randomElement()!
     }
 
-    func latitude(in range: ClosedRange<Float> = -90...90, precision: Int = 4) -> Float {
-        return Float.random(in: range).rounded(toPlaces: precision)
-    }
-
-    func longitude(in range: ClosedRange<Float> = -180...180, precision: Int = 4) -> Float {
-        return Float.random(in: range).rounded(toPlaces: precision)
-    }
-
     func direction(useAbbreviation: Bool = false) -> String {
         if useAbbreviation {
             return self.definitions.direction.randomElement()!
         }
         return self.definitions.directionAbbr.randomElement()!
-    }
-
-    func nearbyGPSCoordinate(coordinate: [Float]? = nil, radius: Float? = nil, isMetric: Bool = false) -> [Float] {
-        func degreesToRadians(degrees: Float) -> Float {
-            return degrees * (Float.pi / 180.0)
-        }
-        func radiansToDegrees(radians: Float) -> Float {
-            return radians * (180.0 / Float.pi)
-        }
-        func kilometersToMiles(miles: Float) -> Float {
-            return miles * 0.621371
-        }
-        func coordinateWithOffset(coordinate: [Float], bearing: Float, distance: Float, isMetric: Bool = false) -> [Float] {
-            let R = 6378.137 as Float // Radius of the Earth (http://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html)
-            let d = isMetric ? distance : kilometersToMiles(miles: distance) // Distance in km
-
-            let lat1 = degreesToRadians(degrees: coordinate[0]) // Current lat point converted to radians
-            let lon1 = degreesToRadians(degrees: coordinate[1]) // Current long point converted to radians
-
-            let lat2 = asin(sin(lat1) * cos(d / R) +
-                cos(lat1) * sin(d / R) * cos(bearing))
-
-            var lon2 = lon1 + atan2(
-                sin(bearing) * sin(d / R) * cos(lat1),
-                cos(d / R) - sin(lat1) * sin(lat2)
-            )
-
-            // Keep longitude in range [-180, 180]
-            if lon2 > degreesToRadians(degrees: 180) {
-                lon2 = lon2 - degreesToRadians(degrees: 360)
-            } else if lon2 < degreesToRadians(degrees: -180) {
-                lon2 = lon2 + degreesToRadians(degrees: 360)
-            }
-
-            return [radiansToDegrees(radians: lat2).distance(to: 4), radiansToDegrees(radians: lon2).distance(to: 4)]
-        }
-
-        guard let coordinate = coordinate else {
-            return [self.latitude(), self.longitude()]
-        }
-
-        let radius = radius ?? 10.0
-
-        return coordinateWithOffset(coordinate: coordinate, bearing: degreesToRadians(degrees: Float.random(in: 0..<1) * 360.0), distance: radius, isMetric: isMetric)
     }
 }
