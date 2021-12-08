382feddfbcf811721b9399268356e96e2a10b8c5
Jacob Graves <jake@hashi.dev>
2021-12-08 11:51:13 -0700

    Adds documentation to the Hacker file.

--- Sources/PhonyKit/Hacker.swift
+++ Sources/PhonyKit/Hacker.swift
@@ -2,27 +2,68 @@
 // Hacker
 
 public extension Phony {
-
+    /// This function returns a random hacker abbreviation.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.hackerAbbreviation()) // TCP
+    ///
+    /// - Returns: An abbreviated hacker string.
     func hackerAbbreviation() -> String {
         self.definitions.hackerAbbreviation.randomElement()!
     }
     
+    /// This function returns a random hacker adjective.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.hackerAdjective()) // redundant
+    ///
+    /// - Returns: An adjective hacker string.
     func hackerAdjective() -> String {
         self.definitions.hackerAdjective.randomElement()!
     }
     
+    /// This function returns a random hacker noun.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.hackerNoun()) // monitor
+    ///
+    /// - Returns: A hacker noun string.
     func hackerNoun() -> String {
         self.definitions.hackerNoun.randomElement()!
     }
 
+    /// This function returns a random hacker verb.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.hackerVerb()) // quantify
+    ///
+    /// - Returns: A hacker verb string.
     func hackerVerb() -> String {
          self.definitions.hackerVerb.randomElement()!
     }
 
+    /// This function returns a random hacker verb that ends in "ing".
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.hackerIngVerb()) // connecting
+    ///
+    /// - Returns: A hacker verb string that ends in "ing".
     func hackerIngVerb() -> String {
         self.definitions.hackerIngVerb.randomElement()!
     }
 
+    /// This function returns a random hacker phrase using concatenation.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.hackerPhrase()) // We need to compress the auxiliary USB transmitter!
+    ///
+    /// - Returns: A random hacker phrase string.
     func hackerPhrase() -> String {
         switch Int.random(in: 0...7) {
         case 0:
