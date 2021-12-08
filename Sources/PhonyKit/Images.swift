bd597a25439d3f5655fc2b0b6a154adc38c7402c
Jacob Graves <jake@hashi.dev>
2021-12-08 13:55:11 -0700

    Adds documentation to the images file.

--- Sources/PhonyKit/Images.swift
+++ Sources/PhonyKit/Images.swift
@@ -13,22 +13,68 @@ public extension Phony {
         case people
     }
 
+    /// This function returns a random image (picsum or unsplash) in String format.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.image()) // https://source.unsplash.com/category/buildings/869x508
+    ///     print(Phony.default.image()) // https://picsum.photos/seed/445A9EC6-B30E-4933-9CF5-0EEA4B813D04/598/489
+    /// - Parameters:
+    ///     - width: An Int representing the desired width of the image
+    ///     - height: An Int representing the desired height of the image
+    /// - Returns: An image url String.
     func image(width: Int = .random(in: 100..<3000), height: Int = .random(in: 100..<3000)) -> String {
         Bool.random() ? self.unsplash(width: width, height: height, keyword: nil) : self.picsum(width: width, height: height)
     }
 
+    /// This function returns a random image (picsum or unsplash) URL.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.image()) // https://source.unsplash.com/category/buildings/869x508
+    ///     print(Phony.default.image()) // https://picsum.photos/seed/445A9EC6-B30E-4933-9CF5-0EEA4B813D04/598/489
+    /// - Parameters:
+    ///     - width: An Int representing the desired width of the image
+    ///     - height: An Int representing the desired height of the image
+    /// - Returns: An image URL.
     func image(width: Int = .random(in: 100..<3000), height: Int = .random(in: 100..<3000)) -> URL {
         Bool.random() ? self.unsplash(width: width, height: height, keyword: nil) : self.picsum(width: width, height: height)
     }
 
+    /// This function returns a random avatar image String.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.avatar()) // https://s3.amazonaws.com/uifaces/faces/twitter/iamgarth/128.jpg
+    ///
+    /// - Returns: An avatar image String.
     func avatar() -> String {
         self.definitions.avatarUri.randomElement()!
     }
 
+    /// This function returns a random avatar image URL.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.avatar()) // https://s3.amazonaws.com/uifaces/faces/twitter/blakesimkins/128.jpg
+    ///
+    /// - Returns: An avatar image URL.
     func avatar() -> URL {
         URL(string: self.avatar())!
     }
 
+    /// This function returns a random unsplash image string.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.unsplash()) // https://source.unsplash.com/category/food/2493x1925
+    ///
+    /// - Parameters:
+    ///     - width: An Int representing the desired width of the image
+    ///     - height: An Int representing the desired height of the image
+    ///     - category: An enum for the type of image requested
+    ///     - keyword: A keyword appended to the url String
+    /// - Returns: An image url String.
     func unsplash(width: Int = .random(in: 100..<3000), height: Int = .random(in: 100..<3000), category: UnsplashCategory = UnsplashCategory.allCases.randomElement()!, keyword: String? = nil) -> String {
         var url = "https://source.unsplash.com/category/\(category)/\(width)x\(height)"
 
@@ -39,18 +85,57 @@ public extension Phony {
         return url
     }
 
+    /// This function returns a random unsplash image URL.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.unsplash()) // https://source.unsplash.com/category/food/2493x1925
+    ///
+    /// - Parameters:
+    ///     - width: An Int representing the desired width of the image
+    ///     - height: An Int representing the desired height of the image
+    ///     - category: An enum for the type of image requested
+    ///     - keyword: A keyword appended to the url String
+    /// - Returns: An image URL.
     func unsplash(width: Int = .random(in: 100..<3000), height: Int = .random(in: 100..<3000), category: UnsplashCategory = UnsplashCategory.allCases.randomElement()!, keyword: String? = nil) -> URL {
         URL(string: self.unsplash(width: width, height: height, category: category, keyword: keyword))!
     }
 
+    /// This function returns a random picsum image in String format.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.picsum()) // https://picsum.photos/seed/445A9EC6-B30E-4933-9CF5-0EEA4B813D04/598/489
+    /// - Parameters:
+    ///     - seed: UUID That holds the image uniqueness.
+    ///     - width: An Int representing the desired width of the image
+    ///     - height: An Int representing the desired height of the image
+    /// - Returns: An image url String.
     func picsum(seed: String = UUID().uuidString, width: Int = .random(in: 100..<3000), height: Int = .random(in: 100..<3000)) -> String {
         "https://picsum.photos/seed/\(seed)/\(width)/\(height)"
     }
 
+    /// This function returns a random picsum image URL.
+    ///
+    /// Usage:
+    ///
+    ///     print(Phony.default.picsum()) // https://picsum.photos/seed/445A9EC6-B30E-4933-9CF5-0EEA4B813D04/598/489
+    /// - Parameters:
+    ///     - seed: UUID That holds the image uniqueness.
+    ///     - width: An Int representing the desired width of the image
+    ///     - height: An Int representing the desired height of the image
+    /// - Returns: An image URL.
     func picsum(seed: String = UUID().uuidString, width: Int = .random(in: 100..<3000), height: Int = .random(in: 100..<3000)) -> URL {
         URL(string: self.picsum(seed: seed, width: width, height: height))!
     }
 
+    /// This function returns an svg String data uri.
+    ///
+    /// - Parameters:
+    ///     - width: An Int representing the desired width of the image
+    ///     - height: An Int representing the desired height of the image
+    ///     - color: String color name.
+    /// - Returns: A String data uri.
     func dataUri(width: Int = .random(in: 100..<3000), height: Int = .random(in: 100..<3000), color: String = "grey") -> String {
         let svgString = "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" baseProfile=\"full\" width=\"\(width)\" height=\"\(height)\"><rect width=\"100%\" height=\"100%\" fill=\"\(color)\"/><text x=\"\(width / 2)\" y=\"\(height / 2)\" font-size=\"20\" alignment-baseline=\"middle\" text-anchor=\"middle\" fill=\"white\">\(width)x\(height)</text></svg>"
         let rawPrefix = "data:image/svg+xml;charset=UTF-8,"
