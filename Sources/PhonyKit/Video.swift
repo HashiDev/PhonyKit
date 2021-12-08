c11e573fb3fecf849708bd4ebafa91427555771a
Jacob Graves <jake@hashi.dev>
2021-12-08 15:53:57 -0700

    Video documentation added.

--- Sources/PhonyKit/Video.swift
+++ Sources/PhonyKit/Video.swift
@@ -4,10 +4,16 @@ import Foundation
 // Video
 
 public extension Phony {
+    /// This function returns a random video URL String.
+    ///
+    /// - Returns: A video URL String.
     func video() -> String {
         self.definitions.videoUris.randomElement()!
     }
 
+    /// This function returns a random video URL.
+    ///
+    /// - Returns: A video URL.
     func video() -> URL {
         URL(string: self.video())!
     }
