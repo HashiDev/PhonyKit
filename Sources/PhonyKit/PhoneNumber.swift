7ebf0e2c8195b9beb7cad049db9be8414bf0bba6
Jacob Graves <jake@hashi.dev>
2021-12-08 15:34:10 -0700

    Adds documentation to a few more pages.

--- Sources/PhonyKit/PhoneNumber.swift
+++ Sources/PhonyKit/PhoneNumber.swift
@@ -1,10 +1,22 @@
 
 // PhoneNumber
 public extension Phony {
+    /// Creates a mock phone number.
+    ///
+    /// - Parameters:
+    ///   - format: A String that is the format of the returned mock phone number. Is optional, will supply one if nil.
+    ///
+    /// - Returns: A mock phone number
     func phoneNumber(format: String? = nil) -> String {
          self.replaceSymbolWithNumber(string: format ?? self.phoneFormat())
     }
 
+    /// Creates a mock phone number.
+    ///
+    /// - Parameters:
+    ///   - index: The index of the format in the definitions file. Default is 0 since that is the most common (Using the DefaultDefinitions).
+    ///
+    /// - Returns: A mock phone number
     func phoneNumber(index: Int = 0) -> String {
         if index > self.definitions.phoneNumberFormats.count {
             return self.phoneNumber(format: self.definitions.phoneNumberFormats[0])
@@ -13,6 +25,9 @@ public extension Phony {
         }
     }
 
+    /// Returns a String phone number format.
+    ///
+    /// - Returns: A phone number format String.
     func phoneFormat() -> String {
         self.definitions.phoneNumberFormats.randomElement()!
     }
